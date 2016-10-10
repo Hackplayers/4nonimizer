85e1c87dde7028648572f6c3f63a329affdd0bda
charly837 <carlos.antonini1@gmail.com>
2016-10-10 11:41:50 -0700

    modify 4nonimizer and add vpn-get-ovpn.sh,vpn-get-pass.sh

--- vpn/freevpn/vpn-get-ovpn.sh
+++ vpn/freevpn/vpn-get-ovpn.sh
@@ -1,6 +1,3 @@
-wget http://freevpn.me/OpenVPN-Certificate-Bundle-Server1.zip
-unzip OpenVPN-Certificate-Bundle-Server1.zip
-cd OpenVPN-Certificate-Bundle-Server1
-mv ./* ../
-rm ./OpenVPN-Certificate-Bundle-Server1.zip
-rm -r ./OpenVPN-Certificate-Bundle-Server1
+
+wget http://freevpn.me/OpenVPN-Certificate-Bundle-Server1.zip 2> /dev/null
+unzip OpenVPN-Certificate-Bundle-Server1.zip && cd OpenVPN-Certificate-Bundle-Server1 && mv ./* ../ && cd ../ && rm ./OpenVPN-Certificate-Bundle-Server1.zip && rm -r ./OpenVPN-Certificate-Bundle-Server1
