350d717178c8da26c0e70b88aa144dd262c7221e
charly837 <carlos.antonini1@gmail.com>
2016-12-07 18:14:09 +0100

    multiple bugs fix

--- vpn/freevpn/vpn-get-pass.sh
+++ vpn/freevpn/vpn-get-pass.sh
@@ -1,2 +1,2 @@
 #4uto
-curl https://freevpn.me/accounts/ 2> /dev/null |  egrep -Eio "Username.*?</li><li><b>Unlimited</b>" | awk '{print $2"\n"$3}' | cut -d'<' -f1 > pass.txt
+curl https://freevpn.me/accounts/ 2> /dev/null | egrep -Eio ">OpenVPN</h3>.*" | egrep -Eio "Username.*?</li><li><b>Unlimited</b>" | awk '{print $2"\n"$3}' | cut -d'<' -f1 > pass.txt
