070e106cbc1c1a36454aed4f2096b7dfed1b32e0
Vicente Motos <vicente.motos@ext.produban.com>
2017-06-09 11:29:15 +0200

    fix vpnbook get credentials function

--- vpn/vpnbook/vpn-get-pass.sh
+++ vpn/vpnbook/vpn-get-pass.sh
@@ -1,3 +1,3 @@
 #4uto
 echo "vpnbook" > /opt/4nonimizer/vpn/vpnbook/pass.txt
-curl www.vpnbook.com/#openvpn 2> /dev/null | grep -E "Password" | awk '{print $2}' | cut -d'<' -f1 | sort -u >> /opt/4nonimizer/vpn/vpnbook/pass.txt
+curl https://www.vpnbook.com/#openvpn 2> /dev/null | grep -E "Password" | awk '{print $2}' | cut -d'<' -f1 | sort -u >> /opt/4nonimizer/vpn/vpnbook/pass.txt
