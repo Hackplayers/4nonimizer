f455ce6883abf45eadab9a6afe33b01b205de6e9
vis0r <hackplayers@ymail.com>
2018-05-17 13:20:40 +0200

    new vpn provider proxy.sh

--- vpn/cyberghost/vpn-get-pass.sh
+++ vpn/cyberghost/vpn-get-pass.sh
@@ -1,2 +1,17 @@
 #m4nual
-echo "This provider does not permit downloading ovpn files in an automatic way."
+read -r -p "Do you have a login for this vpn provider [y/N] " response
+
+case $response in
+    [yY][eE][sS]|[yY]) 
+	read -p 'Username: ' uservar
+	stty -echo
+	read -p 'Password: ' passvar
+	stty echo
+	echo ""
+	echo $uservar > pass.txt && echo $passvar >> pass.txt
+        ;;
+    *)
+        exit 1
+        ;;
+esac
+
