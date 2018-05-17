b1cf8e8a744bc5ed3862dc3a7872c4d534c35fe7
vis0r <hackplayers@ymail.com>
2018-05-17 13:16:04 +0200

    new vpn provider proxy.sh

---
+++ vpn/proxysh/vpn-get-ovpn.sh
@@ -0,0 +1,3 @@
+#m4nual
+echo "This provider does not permit downloading ovpn files in an automatic way."
+echo "Copy ./psh-ovpn-large/Mac, Linux & Android Configs/Individual Servers/*.ovpn in vpn/proxysh dir"
