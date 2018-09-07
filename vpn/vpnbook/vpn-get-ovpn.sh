0226366bfa25cc12eaafab4d74d626b32500eab2
Vicente Motos aka Vis0r <vicente.motos@gmail.com>
2018-09-07 12:08:08 +0200

    Update vpn-get-ovpn.sh

--- vpn/vpnbook/vpn-get-ovpn.sh
+++ vpn/vpnbook/vpn-get-ovpn.sh
@@ -1,5 +1,5 @@
 #4uto
 rm *.ovpn 2> /dev/null
-wget -r -np -l 1 -A zip http://www.vpnbook.com/#openvpn 2> /dev/null
+wget -r -np -l 1 -A zip http://www.vpnbook.com/#openvpn --no-check-certificate 2> /dev/null
 mv ./www.vpnbook.com/free-openvpn-account/*.zip . 
 unzip \*.zip && rm *.zip && rm -rf ./www.vpnbook.com
